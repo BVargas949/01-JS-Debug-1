b89821740bd826245f0e1a4ea0464da934d7bddc
Bryan Vargas <BVargas949@gmail.com>
2019-08-15 18:03:03 -0700

    exercise 2
    Finished up to step 5

--- sub-unit-01-DocumentObjectModel/photos.js
+++ sub-unit-01-DocumentObjectModel/photos.js
@@ -13,6 +13,16 @@
 /* global variables */
 var photoOrder = [1,2,3,4,5];
 
+function populateFigures(){
+   var filename;
+   var currentfig;
+   for(var i= 1; i<4; i++){
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentfig = document.getElementsByTagName("img")[i-1];
+currentfig.src = filename;
+   }
+}
+
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
    for (var i = 0; i < 5; i++) {
@@ -39,8 +49,36 @@ function leftArrow() {
 
 /* open center figure in separate window */
 function zoomFig() {
-   
+   alert("center image clicked");
 }
+
+
+function createEventListeners(){
+
+var leftArrow = document.getElementById("leftarrow");
+var rightArrow = document.getElementById("rightarrow");
+if(leftArrow.addEventListener){
+   leftArrow.addEventListener("click", leftArrow,false);
+}else if(leftArrow.attachEvent){
+   leftArrow.addEventListener("onlclick",leftArrow);
+}
+
+if(rightArrow.addEventListener){
+   rightArrow.addEventListener("click", rightArrow,false);
+}else if(rightArrow.attachEvent){
+   rightArrow.addEventListener("onlclick",rightArrow);
+}
+
+var mainfig = document.getElementsByTagName("img")[1];
+
+if (mainfig.addEventListener) {
+   mainfig.addEventListener("click",zoomFig,false);
+}else if(mainfig.attachEvent){
+   mainfig.addEventListener("click",zoomFig);
+}
+}
+
+
 
 /* create event listeners and populate image elements */
 function setUpPage() {
